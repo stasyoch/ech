ead1be152d3a83f8676b199ed1155ffc5a700330
stasyoch <stasyoch@yandex.ru>
2014-04-21 19:14:11 +0400

    Create task_2.s

---
+++ hw-2/task_2.s
@@ -0,0 +1,35 @@
+.data
+
+fmt:
+        .string "%c"
+fmtp:
+        .string "%d\n"
+int_1:
+        .space 1
+int_2:
+        .space 4
+eof:
+        .string "7"
+
+.text
+.globl main
+main:
+        pushl %ebp
+        movl %esp,%ebp
+        movl $0,%ecx
+ll1:
+        pushl $int_1
+        pushl $fmt
+        call scanf
+        movb int_1,%bl
+        incl %ecx
+        cmpb eof,%bl
+        jne ll1
+        je out
+out:
+        pushl %ecx
+        pushl $fmtp
+        call printf
+        movl %ebp,%esp
+        popl %ebp
+        ret
