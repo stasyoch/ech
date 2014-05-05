149721df8faac278130b228ea488bbdeed1410bd
stasyoch <stasyoch@yandex.ru>
2014-05-05 17:50:17 +0400

    Update task_2.s

--- hw-2/task_2.s
+++ hw-2/task_2.s
@@ -1,35 +1,42 @@
 .data
-
+str_format:
+	.string	"%s\n"
 fmt:
-        .string "%c"
-fmtp:
-        .string "%d\n"
-int_1:
-        .space 1
-int_2:
-        .space 4
-eof:
-        .string "7"
+	.string "%d"
+str_in_1:
+	.string	"12345a"
+str_in_2:
+	.string "12435b"
+.set	str_in_length, . - str_in_1
+.bss
+str_out:
+	.space	str_in_length
 
 .text
-.globl main
+.globl	main
 main:
-        pushl %ebp
-        movl %esp,%ebp
-        movl $0,%ecx
-ll1:
-        pushl $int_1
-        pushl $fmt
-        call scanf
-        movb int_1,%bl
-        incl %ecx
-        cmpb eof,%bl
-        jne ll1
-        je out
-out:
-        pushl %ecx
-        pushl $fmtp
-        call printf
-        movl %ebp,%esp
-        popl %ebp
-        ret
+	pushl	%ebp
+	movl	%esp, %ebp
+	movl	$str_in_1, %esi
+	movl	$str_out, %edi
+	movl	$str_in_length - 1, %ecx
+label:
+	lodsb
+	addb	$5,%al
+	stosb
+		loop	label
+	movsb
+	pushl	$str_out
+	pushl	$str_format
+	call	printf
+	ret scas
+
+l2:
+	pushl $7
+	pushl fmt
+	call printf
+
+	addl	$8, %esp
+	movl	%ebp, %esp
+	popl	%ebp
+	ret
