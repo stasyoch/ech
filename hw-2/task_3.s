e1074084aa292805c07656c6631256e23e5a4598
stasyoch <stasyoch@yandex.ru>
2014-05-15 18:12:51 +0400

    Update task_3.s

--- hw-2/task_3.s
+++ hw-2/task_3.s
@@ -1,24 +1,40 @@
-	.data
-a:
-.float	2.2
-b:
-.float	3.1
-fmt:
-.string "%f\n"
+data
+fmt_scan:
+        .string "%lf%lf%lf"
+fmt_print:
+        .string "%lf\n"
+        .bss
+buf:
+        .space  24
 
-.globl main
-.text
+        .text
+        .globl main
 main:
-pushl	%ebp
-movl	%esp, %ebp
-finit
-flds	a
-flds	b
-faddp
-flds b
-pushl fmt
-call	printf
-addl	$4, %esp
-movl	%ebp, %esp
-popl	%ebp
-ret	
+        pushl %ebp
+        movl %esp, %ebp
+        finit
+        movl $buf, %eax
+        movl $3, %ecx
+step_1:
+        pushl %eax
+        addl $8, %eax
+        loop step_1
+        pushl $fmt_scan
+        call scanf
+        movl $buf, %eax
+        movl $3, %ecx
+        fldl (%eax)
+step_2:
+        addl $8, %eax
+        fldl (%eax)
+        faddp
+        loop step_2
+        fstpl (%esp)
+        wait
+        pushl $fmt_print
+        call printf
+        movl $0, %eax
+        movl %ebp, %esp
+ 	popl %ebp
+        ret
+                  
