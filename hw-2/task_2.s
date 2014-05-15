002c7dea2f3999fe7d6d87e1642c02773ee89227
stasyoch <stasyoch@yandex.ru>
2014-05-15 18:11:29 +0400

    Update task_2.s

--- hw-2/task_2.s
+++ hw-2/task_2.s
@@ -1,42 +1,71 @@
 .data
-str_format:
-	.string	"%s\n"
-fmt:
-	.string "%d"
-str_in_1:
-	.string	"12345a"
-str_in_2:
-	.string "12435b"
-.set	str_in_length, . - str_in_1
-.bss
-str_out:
-	.space	str_in_length
+code:
+        .string "qwertyui"
+str_yes:
+        .string "You win!!"
+str_no:
+        .string "Oops...."
+fmt_scan:
+        .string "%s"
+fmt_print:
+        .string "%s\n"
+str_1:
+        .space 8
+str_2:
+        .space 8
+str_3:
+        .space 8
 
 .text
-.globl	main
+.globl main
 main:
-	pushl	%ebp
-	movl	%esp, %ebp
-	movl	$str_in_1, %esi
-	movl	$str_out, %edi
-	movl	$str_in_length - 1, %ecx
-label:
-	lodsb
-	addb	$5,%al
-	stosb
-		loop	label
-	movsb
-	pushl	$str_out
-	pushl	$str_format
-	call	printf
-	ret scas
-
-l2:
-	pushl $7
-	pushl fmt
-	call printf
-
-	addl	$8, %esp
-	movl	%ebp, %esp
-	popl	%ebp
-	ret
+        pushl %ebp
+        movl %esp, %ebp
+        pushl $str_1
+        pushl $fmt_scan
+        call scanf
+        addl $8, %esp
+        movl $str_1, %esi
+        movl $str_2, %edi
+        movl $8, %ecx
+        rep movsb
+        movl $8, %ecx
+        movl $str_2, %esi
+        movl $code, %edi
+        rep cmpsb
+        jne step_1
+        movl $8, %ecxj
+	movl $str_yes, %esi
+        movl $str_3, %edi
+        rep movsb
+        jmp end
+step_1:
+        movl $8, %ecx
+        movl $str_2, %edi
+        movl $42, %eax
+        repne scasb
+        test %ecx, %ecx
+        je incorrect
+        movl $8, %ecx
+        movl $str_2, %esi
+        movl $str_3, %edi
+step_2:
+        lodsb
+        incb %al
+        stosb
+        loop step_2
+        jmp end
+incorrect:
+        movl $8, %ecx
+        movl $str_no, %esi
+        movl $str_3, %edi
+        rep movsb
+end:
+        pushl $str_3
+        pushl $fmt_print
+        call printf
+        addl $8, %esp
+        movl $0, %eax
+        movl %ebp, %esp
+        popl %ebp
+        ret
