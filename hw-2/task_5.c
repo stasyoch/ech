e78728245615cb0b835ea1fecb624cc35ef97349
stasyoch <stasyoch@yandex.ru>
2014-05-16 11:06:13 +0400

    Update task_5.c

--- hw-2/task_5.c
+++ hw-2/task_5.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+int sum1(int *b,int m)
 float sum2(float *b,int m);
 int main(){
         int i,n,m;
         scanf("%d",&n);
         int *a=(int *)malloc(n*sizeof(int));
         for (i=0;i<n;i++) scanf("%d",&a[i]);
-        printf("%d\n",sum1(a,n));
         float *b=(float *)malloc(n*sizeof(float));
-        for (i=0;i<m;i++) scanf("%f",&b[i]);
-        printf("%f\n",sum2(b,m));
+        for (i=0;i<n;i++) scanf("%f",&b[i]);
+        printf("%d %f\n",sum1(a,n),sum2(b,n));
         return 0;
 }
